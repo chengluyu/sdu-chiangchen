e72c06d086ae5eaac17b16076ba2815609632d12
Cheng Luyu <chengluyu@live.cn>
2017-02-27 20:46:01 +0800

    Add auto login function

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ const UserInput = require('./lib/user-input')
 const Jar = require('./lib/jar')
 const { User, Query } = require('./lib/requests')
 
+async function tryAutoLogin() {
+  const { existsSync, readFileSync } = require('fs')
+
+  let form = existsSync('./auto-login.json')
+      ? JSON.parse(readFileSync('./auto-login.json', 'utf8'))
+      : { username: await UserInput.question('Username: '),
+          password: await UserInput.password('Password') }
+
+  form.verify = await UserInput.question('Capture code: ')
+  return form
+}
+
 async function main() {
   try {
     let jar = new Jar()
@@ -9,11 +21,7 @@ async function main() {
     await User.getPhpSessionID(jar)
     await User.downloadCaptureImage(jar)
     
-    let form = {
-      username: await UserInput.question('Username: '),
-      password: await UserInput.password('Password'),
-      verify: await UserInput.question('Capture code: ')
-    }
+    let form = await tryAutoLogin()
     
     if (await User.login(form, jar)) {
       console.log('Login success')
@@ -22,13 +30,13 @@ async function main() {
       return
     }
     
-    let floorStatus = await Query.getFloorStatus();
-    let selectedArea = await UserInput.chooseFromList('Select an area: ', floorStatus.areas);
-    console.log('You selected: ', selectedArea.name);
+    // let floorStatus = await Query.getFloorStatus();
+    // let selectedArea = await UserInput.chooseFromList('Select an area: ', floorStatus.areas);
+    // console.log('You selected: ', selectedArea.name);
 
-    let areaStatus = await Query.getAreaStatus(selectedArea.id);
-    let selectedSeat = await UserInput.chooseFromList('Select a seat: ', areaStatus.seats);
-    console.log('You selected: ', selectedSeat.name);
+    // let areaStatus = await Query.getAreaStatus(selectedArea.id);
+    // let selectedSeat = await UserInput.chooseFromList('Select a seat: ', areaStatus.seats);
+    // console.log('You selected: ', selectedSeat.name);
 
   } catch (e) {
     console.log(e)
