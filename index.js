09638e9f06991b15987eddfcdece7392487f4a0c
Cheng Luyu <chengluyu@live.cn>
2017-03-01 09:35:45 +0800

    Finish main code, we can book seat now

--- index.js
+++ index.js
@@ -36,6 +36,14 @@ async function main() {
     let selectedSeat = await Input.chooseFromList('Select a seat: ', area.seats)
     console.log('You selected: ', selectedSeat.name)
 
+    let bookResult = await library.bookSeat(selectedSeat, user)
+    if (bookResult.success) {
+      console.log('Login success (message: ${bookResult.serverMessage})')
+    } else {
+      console.log(`Login failed (reason: ${bookResult.serverMessage})`)
+      return
+    }
+
   } catch (e) {
     console.log(e)
   }
