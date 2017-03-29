3c76d591df211a639d85492f4ad9674a67ac6af3
Cheng Luyu <chengluyu@live.cn>
2017-03-29 21:21:09 +0800

    change(server): add responder for '/capturecode'

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // built-in modules
-const fs = require('fs-promise')
+const fs   = require('fs-promise')
+const path = require('path')
 
 // third-party modules
 const Koa          = require('koa')
@@ -16,7 +17,22 @@ let router       = new Router()
 let tokenStorage = new Map()
 
 router.get('/capturecode', async (ctx, next) => {
+  let token = Randomstring.generate(32)
+  let jar = new CookieJar()
 
+  await Requests.getPhpSessionID(jar)
+  const imageData = await Requests.fetchCaptureImage(jar)
+  const base64Data = (new Buffer(imageData)).toString('base64')
+
+  ctx.set('Content-Type', 'application/json')
+  ctx.body = JSON.stringify({
+    token, capture_code: (new Buffer(imageData)).toString('base64')
+  })
+
+  // for debug use
+  // ctx.body = `<html><body><img src="data:image/png;base64,${base64Data}" /></body></html>`
+
+  await next()
 })
 
 router.get('/login', async (ctx, next) => {
@@ -40,3 +56,8 @@ app.use(router.allowedMethods())
 
 app.listen(3000)
 console.log('Server is running on port 3000')
+
+process.on('SIGINT', () => {
+  console.log('\nBye')
+  process.exit()
+})
